1f1ac79754c7d7a7f98a3a25649ff487f11e5f19
Yash Pathak <furiousbluffer@gmail.com>
2022-03-14 19:02:49 +0530

    basic frontend setup.

--- interviews/views.py
+++ interviews/views.py
@@ -1,36 +1,49 @@
 # Create your views here.
 import datetime
 import smtplib
+import time
 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import redirect
 from rest_framework import status
 from rest_framework.generics import ListAPIView, CreateAPIView, \
     UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from interviews.models import Interview
-from interviews.serializers import InterviewSerializer
+from interviews.serializers import InterviewSerializer, NewCustomInterviewSerializer
 from participants.models import Participant
 
 
 class GetInterview(ListAPIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'webapp/dashboard.html'
     queryset = Interview.objects.all()
-    serializer_class = InterviewSerializer
+    serializer_class = NewCustomInterviewSerializer
+
+    def get(self, request):
+        return Response({'interviews': self.get_queryset()})
 
 
 class CreateInterview(CreateAPIView):
+    # renderer_classes = [TemplateHTMLRenderer]
+    # template_name = 'webapp/addinterview.html'
     queryset = Interview.objects.all()
     serializer_class = InterviewSerializer
 
+    # def get(self, request):
+    #     return Response({'serializer': self.serializer_class})
+
     def post(self, request, *args, **kwargs):
         data = request.data
-        if len(data['participants']) < 2:
-            return Response({"status": "error", "message": "A minimum of 2 participants is required"},
-                            status=status.HTTP_406_NOT_ACCEPTABLE)
         if data['end_time'] <= data['start_time']:
-            return Response({"status": "error", "message": "Let the interview begin before ending it"},
-                            status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response("Let the interview begin before ending it")
         serializer = self.serializer_class(data=data, many=False)
         participants = data['participants']
+        if len(participants) < 2:
+            return Response("A minimum of 2 participants is required")
         for i in participants:
             participant = Participant.objects.get(id=i)
             individual_interviews = participant.interview_set.all()
@@ -41,8 +54,7 @@ class CreateInterview(CreateAPIView):
                     'start_time']) or (
                         datetime.datetime.strptime(data['start_time'], '%H:%M').time() <= j['start_time'] and j[
                     'end_time'] <= datetime.datetime.strptime(data['end_time'], '%H:%M').time()):
-                    return Response({'status': "error", 'message': 'Interview Conflict exists'},
-                                    status=status.HTTP_409_CONFLICT)
+                    return Response('Interview Conflict exists')
         if serializer.is_valid():
             smtp_server = "smtp-relay.sendinblue.com"
             port = 587  # For starttls
@@ -62,7 +74,7 @@ class CreateInterview(CreateAPIView):
                 server.sendmail(sender_email, receiver_email, message)
             server.quit()
             serializer.save()
-            return Response(serializer.data)
+            return redirect(to='interviews')
 
 
 class UpdateInterview(UpdateAPIView):
@@ -120,13 +132,12 @@ class UpdateInterview(UpdateAPIView):
 
 class RetrieveInterview(RetrieveAPIView):
     queryset = Interview.objects.all()
-    serializer_class = InterviewSerializer
+    serializer_class = NewCustomInterviewSerializer
 
 
 class DeleteInterview(DestroyAPIView):
     queryset = Interview.objects.all()
     serializer_class = InterviewSerializer
-
 
 # def minimum_participants(sender, **kwargs):
 #     if len(kwargs['pk_set']) < 2:
