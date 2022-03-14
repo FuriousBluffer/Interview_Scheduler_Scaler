a4ca66961bcba221f7ed0e8055d2e75d698f1a47
Yash Pathak <furiousbluffer@gmail.com>
2022-03-14 09:55:49 +0530

    static bug

--- Interview_Scheduler/settings.py
+++ Interview_Scheduler/settings.py
@@ -118,10 +118,10 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'staticfiles/'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
